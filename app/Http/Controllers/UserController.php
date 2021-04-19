3fd7a95fc7e871781f995f41a475b8c39183aed3
Chloe Aemanuelle Susana <susana.chloeaemanuelle@gmail.com>
2021-04-19 14:24:35 +0800

    Changes as of April19 2021

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,17 +3,83 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\User;
+use App\Models\User;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Response;
+use DB;
 
 Class UserController extends Controller {
+    use ApiResponser;
+
     private $request;
 
     public function __construct(Request $request){
         $this->request = $request;
     }
 
-    public function getUsers(){
+    public function getUsers()
+    {
+        $users = DB::connection('mysql')
+        ->select("Select * from tbluser");
+       
+        //return response()->json($users, 200);
+        return $this->successResponse($users);
+    }
+
+    public function index()
+    {
         $users = User::all();
-        return response()->json($users, 200);
+        return $this->successResponse($users);
+    }
+
+    public function add (Request $request)
+    {
+        $rules = [
+            'username' => 'required',
+            'password' => 'required',
+        ];
+
+        $this->validate($request, $rules);
+        $user = User::create($request->all());
+        return $this->successResponse($user, Response::HTTP_CREATED);
+    }
+
+    public function show ($id)
+    {
+        //$user = User::findOrFail($id);
+        $user = User::where('userId', $id)->first();
+        if($user){
+            return $this->successResponse($user);
+        }
+        {
+            return $this->errorResponse('User ID Does Not Exists', Response::HTTP_NOT_FOUND);
+        }
+       
+    }
+
+    public function update (Request $request, $id)
+    {
+        //wla sa koy rules
+
+        $user = User::findOrFail($id);
+        //$user = User::where('userId',$id)->first();
+        //if($user){
+            $user->fill($request->all());
+            if ($user->isClean()){
+          //      return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE);
+            }
+            $user->save();
+            return $this->successResponse($user);
+        //}
+        //{
+            return $this->errorResponse('User ID Does Not Exists', Response::HTTP_NOT_FOUND);
+        //}
+    }
+
+    public function delete ($id)
+    {
+        $user = User::findOrFail($id);
+            $user->delete();
+            return $this->successResponse($user);
     }
 }   
